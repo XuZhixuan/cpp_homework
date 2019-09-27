3139c2b17d7587c222bae2c7fdc656e4960b3110
f(x,z)=xzx <sxxuzhixuan@hotmail.com>
2019-09-27 17:38:33 +0800

    Create 10-2-1-18.cpp

---
+++ src/10-2-1-18.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main()
+{
+	  int input;
+  	std::cin >> input;
+  
+  	int c50 = input / 50;
+  	input = input % 50;
+  
+  	int c20 = input / 20;
+   	input = input % 20;
+  
+  	int c10 = input / 10;
+   	input = input % 10;
+  
+  	int c5 = input / 5;
+  	input = input % 5;
+  
+  	std::cout << c50 << std::endl;
+    std::cout << c20 << std::endl;
+    std::cout << c10 << std::endl;
+    std::cout << c5 << std::endl;
+    std::cout << input << std::endl;
+	
+  	return 0;
+}
