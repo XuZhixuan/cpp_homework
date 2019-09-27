fd1d330bbee04eeb94fb9a511e047317105b574b
f(x,z)=xzx <sxxuzhixuan@hotmail.com>
2019-09-27 17:50:59 +0800

    Update 10-2-1-18.cpp

--- src/10-2-1-18.cpp
+++ src/10-2-1-18.cpp
@@ -2,8 +2,8 @@
 
 int main()
 {
-	  int input;
-  	std::cin >> input;
+	int input;
+	std::cin >> input;
   
   	int c50 = input / 50;
   	input = input % 50;
@@ -18,10 +18,10 @@ int main()
   	input = input % 5;
   
   	std::cout << c50 << std::endl;
-    std::cout << c20 << std::endl;
-    std::cout << c10 << std::endl;
-    std::cout << c5 << std::endl;
-    std::cout << input << std::endl;
+    	std::cout << c20 << std::endl;
+    	std::cout << c10 << std::endl;
+    	std::cout << c5 << std::endl;
+    	std::cout << input << std::endl;
 	
   	return 0;
 }
