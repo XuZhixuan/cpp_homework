6f6494628f0ec145f994692011ef2dae90e0d368
f(x,z)=xzx <sxxuzhixuan@hotmail.com>
2019-10-18 16:31:06 +0800

    Create 10-4-1-38.cpp

---
+++ src/10-4-1-38.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int sum(int*, int);
+
+int main()
+{
+	int length;
+
+	cin >> length;
+
+	length += 1;
+
+	int *feb = new int[length];
+
+	feb[0] = 0;
+	feb[1] = 1;
+
+	for (int i = 2; i < length; i++)
+	{
+		feb[i] = feb[i - 1] + feb[i - 2];
+	}
+
+	cout << feb[length - 1] << endl;
+	cout << sum(feb, length) << endl;
+
+	return 0;
+}
+
+int sum(int* arr, int size)
+{
+	int sum = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		sum += arr[i];
+	}
+
+	return sum;
+}
