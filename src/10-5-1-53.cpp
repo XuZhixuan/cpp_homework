bef241c22b36aca49d3ff5ddb5e8e1a60b96cf6f
f(x,z)=xzx <sxxuzhixuan@hotmail.com>
2019-10-20 17:08:21 +0800

    Create 10-5-1-53.cpp

---
+++ src/10-5-1-53.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ack(int, int);
+
+int main()
+{
+    int m, n;
+
+    cin >> m >> n;
+
+    cout << ack(m, n) << endl;
+
+    return 0;
+}
+
+int ack(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+
+    if (m > 0)
+    {
+        if (n == 0)
+        {
+            return ack(m - 1, 1);
+        }
+        
+        if (n > 0)
+        {
+            return ack(m - 1, ack(m, n - 1));
+        }        
+    }    
+    
+    return -1;
+}
