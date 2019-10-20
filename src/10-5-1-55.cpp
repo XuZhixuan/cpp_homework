6fe82e56f867443780c4448e4e532ece67985457
f(x,z)=xzx <sxxuzhixuan@hotmail.com>
2019-10-20 17:11:12 +0800

    Create 10-5-1-55.cpp

---
+++ src/10-5-1-55.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int cubic(int);
+double cubic(double);
+
+int main()
+{
+    int a;
+
+    double b;
+
+    cin >> a >> b;
+
+    cout << cubic(a) << endl;
+    cout << cubic(b) << endl;
+
+    return 0;
+}
+
+int cubic(int in)
+{
+    return in * in * in;
+}
+
+double cubic(double in)
+{
+    return in * in * in;    
+}
