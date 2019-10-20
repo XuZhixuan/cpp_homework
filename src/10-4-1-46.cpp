6064ba56b4ce9db16a6fdab3666100afc9d1360f
f(x,z)=xzx <sxxuzhixuan@hotmail.com>
2019-10-20 16:28:49 +0800

    Update 10-4-1-46.cpp

--- src/10-4-1-46.cpp
+++ src/10-4-1-46.cpp
@@ -1,3 +1,10 @@
+/**
+ * Author: XuZhixuan
+ * E-mail: sxxuzhixuan@hotmail.com
+ * 
+ * 由于未知原因，本题暂时无法通过
+ **/
+
 #include <iostream>
 #include <string>
 
@@ -10,6 +17,9 @@ struct Contact
 };
 
 Contact transformer(string);
+int findNum(string);
+void sort(Contact*, int);
+bool higher(string, string);
 
 int main()
 {
@@ -22,20 +32,74 @@ int main()
 		c[i] = transformer(input);
 	}
 
-	// TODO 完成排序函数与输出
+	sort(c, 5);
 
+    for (int i = 4; i >= 0; i--)
+    {
+        cout << c[i].name << ' ' << c[i].phone << endl;
+    }
+    
 	return 0;
+}
+
+bool higher(string a, string b)
+{
+    int length = min(a.length(), b.length());
+
+    for (int i = 0; i < length; i++)
+    {
+        if (a[i] < b[i])
+        {
+            return true;
+        } else if (a[i] > b[i])
+        {
+            return false;
+        }
+        
+    }
+
+    return false;    
+}
+
+
+void sort(Contact* contacts, int size)
+{
+    Contact temp;
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (higher(contacts[j].name, contacts[j + 1].name))
+            {
+                temp = contacts[j];
+                contacts[j] = contacts[j + 1];
+                contacts[j + 1] = temp;
+            }            
+        }        
+    }    
 }
 
 Contact transformer(string origin)
 {
-	char separator = ' ';
 	int pos = 0;
 	Contact result;
 
-	pos = origin.find(separator, pos);
+	pos = findNum(origin) - 1;
 	result.name = origin.substr(0, pos);
 	result.phone = origin.substr(pos + 1, origin.length());
 
 	return result;
+}
+
+int findNum(string input)
+{
+    for (int i = 0; i < input.size(); i++)
+    {
+        if (input[i] > 47 && input[i] < 58)
+        {
+            return i;
+        }        
+    }
+    return -1; 
 }
