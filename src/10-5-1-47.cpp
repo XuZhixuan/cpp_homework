9b7b9e9074f9babb69deb433c7ff453078e2e492
f(x,z)=xzx <sxxuzhixuan@hotmail.com>
2019-10-20 16:49:23 +0800

    Create 10-5-1-47.cpp

---
+++ src/10-5-1-47.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+void mystrrev(char string[]);
+
+int main()
+{
+    char* input = new char[100];
+
+    cin.getline(input, 99);
+
+    mystrrev(input);
+
+    cout << input << endl;
+
+    return 0;
+}
+
+void mystrrev(char string[])
+{
+    int length = strlen(string);
+    int count = length / 2;
+
+    char temp;
+
+    for (int i = 0; i < count; i++)
+    {
+        temp = string[i];
+        string[i] = string[length - 1 - i];
+        string[length - 1 - i] = temp;
+    }
+
+    return;    
+}
