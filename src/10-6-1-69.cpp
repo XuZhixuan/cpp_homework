6623bd8b86b214656420924f0d1472240098090f
f(x,z)=xzx <sxxuzhixuan@hotmail.com>
2019-10-20 22:49:54 +0800

    Create 10-6-1-69.cpp

---
+++ src/10-6-1-69.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int** readMatrix(int, int);
+void printMatrix(int**, int, int);
+int** transposeMatrix(int**, int, int);
+void deleteMatrix(int**, int, int);
+
+int main()
+{
+    int **mat = readMatrix(4, 4);
+
+    int **trans_mat = transposeMatrix(mat, 4, 4);
+
+    printMatrix(trans_mat, 4, 4);
+
+    deleteMatrix(trans_mat, 4, 4);
+
+	return 0;
+}
+
+void printMatrix(int** matrix, int row, int col)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout << matrix[i][j] << (j == col - 1 ? '\n' : ' ');
+        }
+    }    
+}
+
+int** transposeMatrix(int** origin_matrix, int row, int col)
+{
+    int **matrix = new int*[col];
+
+    for (int i = 0; i < col; i++)
+    {
+        matrix[i] = new int[row];
+    }
+
+    for (int i = 0; i < col; i++)
+    {
+        for (int j = 0; j < row; j++)
+        {
+            matrix[i][j] = origin_matrix[j][i];
+        }        
+    }
+
+    deleteMatrix(origin_matrix, row, col);
+
+    return matrix;    
+}
+
+int** readMatrix(int row, int col)
+{
+    int **matrix = new int*[row];
+
+    for (int i = 0; i < row; i++)
+    {
+        matrix[i] = new int[col];
+    }    
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cin >> matrix[i][j];
+        }
+        
+    }
+
+    return matrix;    
+}
+
+void deleteMatrix(int** matrix, int row, int col)
+{
+    for (int i = 0; i < row; i++)
+    {
+        delete[] matrix[i];
+    }
+
+    delete[] matrix;
+}
